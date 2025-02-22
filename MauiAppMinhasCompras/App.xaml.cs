6b4789876ec1fdd99db83f9e0b47a15b6797d769
winnetayna <winnetayna92@gmail.com>
2025-02-22 15:08:57 -0300

    Add models e troca da tela inicial.

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,15 +1,19 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Resources.Views;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
+        protected override Window CreateWindow(IActivationState? activationState) // A interrogação(?) significa que marquei como anulável, o parâmetro activationState.
         {
-            return new Window(new AppShell());
+            return new Window(new NavigationPage(new ListaProduto())); // Usando a classe ListaProduto
         }
     }
 }
+
